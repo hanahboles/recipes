16aad151857b047b51242aa6ea19177bb19e6812
Hanah Boles <58400558+hanahboles@users.noreply.github.com>
2023-11-28 14:37:37 -0700

    switch from browser router to hash router for git hub pages support

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import RecipeDetails from './pages/RecipeDetails';
 import RecipeList from './pages/RecipeList';
 import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
